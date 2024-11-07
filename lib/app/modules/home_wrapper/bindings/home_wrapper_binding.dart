89b1353658176469e5976487a8f54045c21c958c
abel3ri <abelmerete@yahoo.com>
2024-11-07 15:05:23 +0300

    feat: improve home wrapper

--- lib/app/modules/home_wrapper/bindings/home_wrapper_binding.dart
+++ lib/app/modules/home_wrapper/bindings/home_wrapper_binding.dart
@@ -1,3 +1,6 @@
+import 'package:business_dir/app/modules/favorite/controllers/favorite_controller.dart';
+import 'package:business_dir/app/modules/profile/controllers/profile_controller.dart';
+import 'package:business_dir/app/modules/search/controllers/search_controller.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_wrapper_controller.dart';
@@ -6,5 +9,8 @@ class HomeWrapperBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<HomeWrapperController>(() => HomeWrapperController());
+    Get.lazyPut<SearchController>(() => SearchController());
+    Get.lazyPut<FavoriteController>(() => FavoriteController());
+    Get.lazyPut<ProfileController>(() => ProfileController());
   }
 }
--- lib/app/modules/home_wrapper/controllers/home_wrapper_controller.dart
+++ lib/app/modules/home_wrapper/controllers/home_wrapper_controller.dart
@@ -1,27 +1,33 @@
+import 'package:business_dir/app/modules/favorite/controllers/favorite_controller.dart';
 import 'package:business_dir/app/modules/search/controllers/search_controller.dart';
 import 'package:get/get.dart';
 
 class HomeWrapperController extends GetxController {
   Rx<int> index = 0.obs;
   late SearchController searchController;
+  late FavoriteController favoriteController;
 
   @override
   void onInit() {
     super.onInit();
     searchController = Get.find<SearchController>();
+    favoriteController = Get.find<FavoriteController>();
   }
 
   void onPageChanged(int i) {
-    if (i != 1 && Get.focusScope!.hasFocus) {
-      Get.focusScope!.unfocus();
+    if (i != 1) {
+      if (Get.focusScope?.hasFocus ?? false) {
+        Get.focusScope!.unfocus();
+      }
+
       searchController.searchResults.value = [];
       searchController.searchInputController.text = "";
-    }
-    index.value = i;
-    if (i == 1) {
-      searchController.animateSearchLottie(true);
+      searchController.isLoading.value = false;
+      searchController.animateSearchLottie.value = false;
     } else {
-      searchController.animateSearchLottie(false);
+      searchController.animateSearchLottie.value = true;
     }
+
+    index.value = i;
   }
 }
