65345a43665d178cc89caaf8355c698d71ca34e2
abel3ri <abelmerete@yahoo.com>
2024-11-07 15:09:04 +0300

    feat: improve search module

--- lib/app/modules/search/controllers/search_controller.dart
+++ lib/app/modules/search/controllers/search_controller.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:business_dir/app/data/models/app_error_model.dart';
 import 'package:business_dir/app/data/models/business_model.dart';
+import 'package:business_dir/app/data/providers/auth_provider.dart';
+import 'package:business_dir/utils/error_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:get/get.dart';
@@ -16,14 +18,20 @@ class SearchController extends GetConnect {
   @override
   onInit() {
     super.onInit();
-    // httpClient.baseUrl = "http://192.168.22.202:8000/api/v1";
-    httpClient.baseUrl = 'http://10.0.2.2:8000/api/v1';
+    httpClient.baseUrl = "https://businessdirectory-vnct9q98.b4a.run/api/v1";
+    // httpClient.baseUrl = 'http://10.0.2.2:8000/api/v1';
   }
 
   Future<Either<AppErrorModel, void>> searchBusiness({
     required String query,
   }) async {
     try {
+      final token =
+          await Get.find<AuthProvider>().secureStorage.read(key: "jwtToken");
+      if (query.isEmpty) {
+        return right(null);
+      }
+
       if (_debounce?.isActive ?? false) {
         _debounce?.cancel();
       }
@@ -31,21 +39,27 @@ class SearchController extends GetConnect {
       _debounce = Timer(Duration(milliseconds: 500), () async {
         try {
           isLoading.value = true;
-          final res = await get("/businesses/search?q=${query}");
-          if (res.hasError) {
-            throw "Unknown Error";
-          }
+          final res = await get(
+            "/businesses/search?q=$query",
+            headers: {"Authorization": "Bearer $token"},
+          );
+
+          handleError(res);
+
           searchResults.value = List.from(res.body['data']).map((business) {
             return BusinessModel.fromJson(business);
           }).toList();
+
           if (query.isEmpty) {
             searchResults.value = [];
           }
-          isLoading.value = false;
         } catch (e) {
-          throw e.toString();
+          print("Error during search: $e");
+        } finally {
+          isLoading.value = false;
         }
       });
+
       return right(null);
     } catch (e) {
       return left(AppErrorModel(body: e.toString()));
@@ -56,6 +70,7 @@ class SearchController extends GetConnect {
 
   @override
   void onClose() {
+    _debounce?.cancel();
     searchInputController.dispose();
     super.onClose();
   }
