bf681544e72218611e1eded55fe0540557e8fa3e
abel3ri <abelmerete@yahoo.com>
2024-11-07 14:51:44 +0300

    feat: improve category module

--- lib/app/modules/category/bindings/category_binding.dart
+++ lib/app/modules/category/bindings/category_binding.dart
@@ -1,3 +1,5 @@
+import 'package:business_dir/app/data/providers/favorite_provider.dart';
+import 'package:business_dir/app/modules/favorite/controllers/favorite_controller.dart';
 import 'package:get/get.dart';
 
 import '../controllers/category_controller.dart';
@@ -5,8 +7,8 @@ import '../controllers/category_controller.dart';
 class CategoryBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<CategoryController>(
-      () => CategoryController(),
-    );
+    Get.lazyPut<CategoryController>(() => CategoryController());
+    Get.lazyPut(() => FavoriteController());
+    Get.lazyPut(() => FavoriteProvider());
   }
 }
